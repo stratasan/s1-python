9f8ec4ea245e68bd64bf7c6c97c9a3d2adc6aa8b
Scott Burns <scott.s.burns@gmail.com>
2020-06-01 21:43:10 -0500

    fix test name

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -13,7 +13,7 @@ def test_parsing_invalid_length_no_repeat(bad_line, basic_spec):
 @pytest.mark.parametrize(
     "bad_line", ["1|foo", "1|foo|bar|bat|zoo", "1|foo|bar|bat|zoo|zod|zep"]
 )
-def test_parsing_invalid_length_no_repeat(bad_line, repeat_spec):
+def test_parsing_invalid_length_repeat_spec(bad_line, repeat_spec):
     with pytest.raises(InvalidFieldLength):
         parse_line_with_spec(bad_line, repeat_spec)
 
